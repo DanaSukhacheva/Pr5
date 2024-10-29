36467528c97d64036af2ab20149e7fb3adcac710
dana <danask2006@gmail.com>
2024-10-29 19:14:26 +0700

    first commit

---
+++ Helper.cs
@@ -0,0 +1,50 @@
+﻿using Pr5BB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr5BB
+{
+   
+    public class Helper
+    {
+        private static databaseEntities _context;
+        public static databaseEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new databaseEntities();
+            }
+            return _context;
+
+        }
+        public void CreateUser(Authorization user)
+        {
+            _context.Authorization.Add(user); 
+            _context.SaveChanges(); 
+        }
+        public void UpdateAuthorization(Authorization user)
+        {
+            _context.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges(); 
+        }
+        public void RemoveUser(int idAuthorization)
+        {
+            var users = _context.Authorization.Find(idAuthorization); 
+            _context.Authorization.Remove(users); 
+            _context.SaveChanges(); 
+        }
+        public List<Authorization> FiltrUsers()
+        {
+            return _context.Authorization.Where(x => x.login.StartsWith("M") || x.login.StartsWith("A")).ToList();
+        }
+        public List<Authorization> SortUsers()
+        {
+            return _context.Authorization.OrderBy(x => x.login).ToList();
+        }
+      
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using HashPasswords;
+using Pr5BB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr5BB
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            databaseEntities db = Helper.GetContext();
+            Class1 hasher = new Class1();
+            /*var jobtitle = db.Jobtitles.FirstOrDefault();
+            Console.WriteLine(jobtitle.title + " " + jobtitle.ID_jobtitle.ToString());*/
+           
+            string hashpassword = hasher.HashPassword("Hr46hI2m");
+            Console.WriteLine("Оригинальный пароль: " + "Hr46hI2m");
+            var personalData = db.Personal_data.Where(a => a.gmail == "zellen012@gmail.com").FirstOrDefault();
+            var employee = db.Employees.Where(a => a.Personal_data.ID_personal_data == personalData.ID_personal_data);
+            Authorization authorization = new Authorization {
+                ID_authorization = 6,
+                login = "meneger",
+                password = "Hr46hI2m"
+            };
+            db.Authorization.Add(authorization);
+            db.SaveChanges();
+            
+            
+        }
+    }
+}
